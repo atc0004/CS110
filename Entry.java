faece9f230ba295dc3a2524001b51192962bfdea
Austin <at.cottrill@gmail.com>
2017-11-09 11:33:06 -0500

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -5,12 +5,23 @@ public class Entry {
 	private final String LAST_NAME;
 	private final String PHONE_NUMBER;
 
+	/**
+	 * Constructor that creates an entry in the phone book.
+	 * @param fName First name of the entry
+	 * @param lName Last name of the entry
+	 * @param num Phone number of the entry
+	 */
 	public Entry(String fName, String lName, String num) {
 		this.FIRST_NAME = fName;
 		this.LAST_NAME = lName;
 		this.PHONE_NUMBER = num;
 	}
 
+	/**
+	 * 
+	 * @param otherEntry The entry that this entry will be compared to
+	 * @return -1 if this needs to come before other, 1 if it needs to come after other
+	 */
 	public int compareTo(Entry otherEntry) {
 		int value = new String(LAST_NAME).compareTo(otherEntry.LAST_NAME);
 		switch (value) {
@@ -25,6 +36,10 @@ public class Entry {
 		}
 	}
 
+	/**
+	 * Creates a string of the entry
+	 * @return The entry as a string
+	 */
 	public String toCSV() {
 		String entry;
 		entry = (LAST_NAME + "," + FIRST_NAME + "," + PHONE_NUMBER);
